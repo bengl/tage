6aac7a57c71e09348cd328654eeb2c7f772be902
Bryan English <bryan@bryanenglish.com>
2020-03-07 16:28:52 -0500

    ensure ordinary array inputs don't trigger the template tag

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ See LICENSE.txt
 */
 'use strict'
 
+function isTemplateArgs (arg0) {
+  return Array.isArray(arg0) && Array.isArray(arg0.raw)
+}
 module.exports = orig => function (str, ...subs) {
-  if (Array.isArray(str)) {
+  if (isTemplateArgs(str)) {
     return (...args) => orig.call(this, String.raw(str, ...subs), ...args)
   } else {
     return orig.apply(this, arguments)
--- test.js
+++ test.js
@@ -14,6 +14,7 @@ const bop = {
   toString: () => 'bop',
   wrapped
 }
+const ordinaryArray = Object.assign([], { toString: () => '[arr]' })
 
 test`function works as normal`(() => {
   equal(wrapped('foo', 'bar', 'baz'), 'foo bar baz ' + topThis)
@@ -29,6 +30,10 @@ test`function works with template string`(() => {
 
 test`function works with template string with context`(() => {
   equal(bop.wrapped`foo`('bar', 'baz'), 'foo bar baz bop')
+})
+
+test`function does not assume tagged for ordinary array 1st arg`(() => {
+  equal(wrapped(ordinaryArray, 'foo', 'bar'), '[arr] foo bar ' + topThis)
 })
 
 test()
